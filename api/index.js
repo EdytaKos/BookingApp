b7a793bd72ca98732061e94d5ee20a4e8d16d385
Paula430 <paulakawalec8@gmail.com>
2023-04-10 13:50:59 +0200

    naprawa rejestracji

--- api/index.js
+++ api/index.js
@@ -2,29 +2,55 @@
 //Hasło: nFKiapngQ6dn3aoF
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config();
-const app = express();
+const bcrypt = require("bcryptjs");
+const User = require("./models/User.js");
 const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+const bcryptSalt = bcrypt.genSaltSync(8);
 
 app.use(express.json());
 
 app.use(
   cors({
     credentials: true,
-    origin: "http://127.0.0.1:5173/",
+    origin: "http://127.0.0.1:5173",
   })
 );
 
-mongoose.connect(process.env.MONGO_URL);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("connected to database"))
+  .catch((e) => console.log(e));
 
 app.get("/test", (req, res) => {
   res.json("test ok");
 });
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
 
-  res.json({ name, email, password });
+  try {
+    const newUser = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(newUser);
+  } catch {
+    res.status(422).json(e);
+  }
 });
 
 app.listen(4000);
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,20 +1,36 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function LoginPage(){
-    return (
-        <div className="mt-4 grow flex items-center justify-around" >
-            <div className="mb-64">
-            <h1 className="text-4xl text-center">Logowanie</h1>
-            <form className="max-w-md mx-auto">
-                <input type="email" placeholder='moj@email.com' />
-                <input type="password" placeholder="Hasło"/>
-                <button className="primary">Zaloguj</button>
-                <div className="text-center py-2 text-gray-500"> Nie masz jeszcze konta?    
-                    <Link className="underline" to={'/register'}> Załóż je tutaj!</Link> 
-                </div>
-            </form>
-            </div>
-         
-        </div>
-    )
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  async function handleLoginSubmit(ev) {
+    ev.preventDefault();
+    try {
+      await axios.post("/login", { email, password });
+      alert("Login successful");
+    } catch (e) {
+      alert("Login failed");
+    }
+  }
+  return (
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-64">
+        <h1 className="text-4xl text-center">Logowanie</h1>
+        <form className="max-w-md mx-auto">
+          <input type="email" placeholder="moj@email.com" />
+          <input type="password" placeholder="Hasło" />
+          <button className="primary">Zaloguj</button>
+          <div className="text-center py-2 text-gray-500">
+            {" "}
+            Nie masz jeszcze konta?
+            <Link className="underline" to={"/register"}>
+              {" "}
+              Załóż je tutaj!
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
