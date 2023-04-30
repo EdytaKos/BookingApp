54798e07a13fb3a27aff65bbdffdd466f157b60b
Paula430 <paulakawalec8@gmail.com>
2023-04-30 13:14:51 +0200

    dodawanie plikow z urzadzenia

--- client/src/pages/NewPlacePage.jsx
+++ client/src/pages/NewPlacePage.jsx
@@ -41,6 +41,25 @@ export default function NewPlacePage() {
     setPhotoLink("");
   }
 
+  function uploadPhoto(ev) {
+    const files = ev.target.files;
+    const data = new FormData();
+    for (let i = 0; i < files.length; i++) {
+      data.append("photos", files[i]);
+    }
+
+    axios
+      .post("/upload", data, {
+        headers: { "Content-type": "multipart/form-data" },
+      })
+      .then((res) => {
+        const { data: filenames } = res;
+        setAddedPhotos((prev) => {
+          return [...prev, ...filenames];
+        });
+      });
+  }
+
   return (
     <div>
       <form>
@@ -79,15 +98,22 @@ export default function NewPlacePage() {
         <div className="mt-2 grid gap-2 grid-cols-3 md:grid-col-4 lg:grid-cols6">
           {addedPhotos.length > 0 &&
             addedPhotos.map((link) => (
-              <div>
+              <div className="h-32 flex">
                 <img
-                  className="rounded-2xl"
+                  className="rounded-2xl w-full object-cover"
                   src={"http://localhost:4000/uploads/" + link}
                   alt=""
                 />
               </div>
             ))}
-          <button className="flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
+
+          <label className="h-32 cursor-pointer flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
+            <input
+              type="file"
+              multiple
+              className="hidden"
+              onChange={uploadPhoto}
+            />
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -102,8 +128,8 @@ export default function NewPlacePage() {
                 d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
               />
             </svg>
-            Wrzuć plik
-          </button>
+            Wrzuć pliki
+          </label>
         </div>
         {preInput("Opis", "Opis miejsca")}
         <textarea
